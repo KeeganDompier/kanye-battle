6b7166dc810278bcc240a732a63229c002c1b928
KeeganDompier <58610698+KeeganDompier@users.noreply.github.com>
2020-01-17 14:03:00 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,228 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+5 5 d d 5 5 d d 5 5 d d 5 5 d d 
+5 5 d d 5 5 d d 5 5 d d 5 5 d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+d d d d d d d d d d d d d d d d 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d 5 5 d d d d d f f 
+d d d d d d d 5 5 d d d d d f f 
+d d d d d d d d d d d d d d f f 
+5 5 d d 5 5 d d d d d d d d f f 
+5 5 d d 5 5 d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d d d d d d d d f f 
+f f d d d d d d d d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d d d d d d d d f f 
+f f d d d d d d d d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d d d d d d d d f f 
+f f d d d d d d d d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d d d d d d d d f f 
+f f d d d d d d d d d d d d f f 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d 5 5 d 5 5 d d 
+f f d d d d d d d 5 5 d 5 5 d d 
+f f d d d d d 5 5 d d d d d d d 
+f f d d d d d 5 5 d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d 5 5 d d d d d d d 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d d d d d d d d f f 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d 5 5 d d d d d f f 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d 5 5 d d d d d d d 
+f f d d d d d 5 5 d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d 5 5 d d 5 5 d d 
+f f d d d d d d 5 5 d d 5 5 d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f d d d d d d d d d d d d d d 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+5 5 d d 5 5 d d d d d d d d f f 
+5 5 d d 5 5 d 5 5 d d d d d f f 
+d d d d d d d 5 5 d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d d d d d d d d f f 
+d d d d d d d 5 5 d d d d d f f 
+d d d d d d d 5 5 d d d d d f f 
+f f d d d d d d d d d d d d f f 
+f f d d d d d d d d d d d d f f 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+`
+    //% blockIdentity=images._tile
+    export const tile8 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+}
+function walls () {
+    tiles.setWallAt(tiles.getTileLocation(3, 2), true)
+    tiles.setWallAt(tiles.getTileLocation(4, 2), true)
+    tiles.setWallAt(tiles.getTileLocation(2, 2), true)
+    tiles.setWallAt(tiles.getTileLocation(4, 4), true)
+    tiles.setWallAt(tiles.getTileLocation(4, 5), true)
+    tiles.setWallAt(tiles.getTileLocation(4, 6), true)
+    tiles.setWallAt(tiles.getTileLocation(4, 0), true)
+    tiles.setWallAt(tiles.getTileLocation(1, 2), true)
+    tiles.setWallAt(tiles.getTileLocation(2, 1), true)
+    tiles.setWallAt(tiles.getTileLocation(1, 1), true)
+}
+function Tilemap () {
+    tiles.setTilemap(tiles.createTilemap(
+            hex`0a0008000401010101010101010603070707070707070703030707070707070707030501010101080707070307070707070707070703070707070707070707030707070707070707070301010101010101010102`,
+            img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8],
+            TileScale.Sixteen
+        ))
+}
+function Rapper () {
+    Rapper2 = sprites.create(img`
+. . . . . e e e e e e . . . . . 
+. . . . . e f e e f e . . . . . 
+. . . . . e e e e e e . . . . . 
+. . . . . e e f f e e . . . . . 
+. . . . . . . e e . . . . . . . 
+. . . . e f 5 f f 5 f e . . . . 
+. . . e e f f 5 5 f f e e . . . 
+. . . e e . f f f f . e e . . . 
+. . . e e . f f f f . e e . . . 
+. . . e . . f f f f . . e . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f . . f . . . . . . 
+. . . . . . f . . f . . . . . . 
+. . . . . . f . . f . . . . . . 
+. . . . . . f . . f . . . . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.Player)
+    controller.moveSprite(Rapper2)
+    Rapper2.setPosition(10, 119)
+}
+let Rapper2: Sprite = null
+Tilemap()
+Rapper()
+walls()
